7a975ec77783cfaffb8d6b2fe3c98e8a1ebc543e
nffgnfg <djfduvbxs@gmail.com>
2022-11-18 16:17:32 +0300

    mahmounjd

--- connect/connect.js
+++ connect/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/eze')
+mongoose.connect('mongodb+srv://mahoud:mahmood1234@cluster0.k5feipn.mongodb.net/?retryWrites=true&w=majority')
      .then(
      ()=>{
       console.log('connect');
