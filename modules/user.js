1471af9154d8d74ad7aedc70b2b82132763f9dd3
nffgnfg <djfduvbxs@gmail.com>
2022-12-11 17:35:58 +0300

    first commit

--- modules/user.js
+++ modules/user.js
@@ -2,8 +2,13 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User',
 {
 name: {type:String},
-last: {type:String},
-apw:{type:String}
+desc: {type:String},
+numberproduct: {type:String},
+sizeproduct: {type:String},
+prase: {type:String},
+expar: {type:String},
+image: {type:String},
+conutry:{type:String}
 }
 )
 
